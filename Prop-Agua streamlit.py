6e3ea427e70320cd32969e89e310f62bc713d3fe
Pablo <pablo.sobral@gmail.com>
2025-08-09 23:11:48 -0300

    Update Prop-Agua streamlit.py

--- Prop-Agua streamlit.py
+++ Prop-Agua streamlit.py
@@ -6,8 +6,8 @@ from CoolProp.CoolProp import PropsSI
 from itertools import combinations
 
 # Título de la aplicación
-st.caption("##### Termodinámica – Máquinas Térmicas – Tecnología del Calor")
-st.markdown("#### **💧 Propiedades del agua**")
+st.caption("###### Termodinámica – Máquinas Térmicas – Tecnología del Calor")
+st.markdown("### **💧 Propiedades del agua**")
 st.divider()
 
 # Variables disponibles
