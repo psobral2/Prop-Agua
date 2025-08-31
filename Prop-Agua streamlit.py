7a34ef3d2a52e3998ea33057acad656580c24049
Pablo <pablo.sobral@gmail.com>
2025-08-31 18:17:15 -0300

    Update Prop-Agua streamlit.py

--- Prop-Agua streamlit.py
+++ Prop-Agua streamlit.py
@@ -9,16 +9,6 @@ from itertools import combinations
 st.set_page_config(page_title="Propiedades de los Fluidos", page_icon="🌡️")
 st.caption("###### Termodinámica – Máquinas Térmicas – Tecnología del Calor")
 
-# Para forzar 2 columnas en pantalla de celulares
-st.markdown("""
-<style>
-[data-testid="stHorizontalBlock"] > div {
-    flex: 1 1 50%;
-    min-width: 0;
-}
-</style>
-""", unsafe_allow_html=True)
-
 # Selección de fluido
 FLUID_MAP = {
     'Agua': 'Water',
